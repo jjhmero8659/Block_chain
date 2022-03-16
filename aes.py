4f54e5113b57485f54d96e415c37fc4fd9de1e4e
jjhmero8659 <77915673+jjhmero8659@users.noreply.github.com>
2022-03-16 23:50:01 +0900

    Add files via upload

--- aes.py
+++ aes.py
@@ -1,36 +1,97 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 from cryptography.hazmat.primitives import serialization
 
-pem = private_key.private_bytes(
-    enconding = serialization.Encoding.PEM,
-    format = serialization.PrivateFormat.PKCS8,
-    encryption_algorithm = serialization.NoEncryption()
-    )
-with open('private_key.pem','wb')as f:
-    f.write(pem)
+message = input()
 
-## 개인 키를 파일에 저장
+message = message.encode()
+print(message)
 
-with open("private_key.pem","rb")as key_file:
+with open("private_key.pem","rb")as key_file: ##개인 키 
     private_key = serialization.load_pem_private_key(
         key_file.read(),
         password=None,
         backend=default_backend()
     )
-##파일에 저장된 개인 키를 읽어오기
-
-pem = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
-
-with open('public_key.pem','wb')as f:
-    f.write(pem)
-##공개 키를 파일에 저장
-
-with open("public_key.pem","rb")as key_file:
-    publick_key = serialization.load_pem_public_key(
+ 
+with open("public_key.pem","rb")as key_file: ## 공유키
+    public_key = serialization.load_pem_public_key(
         key_file.read(),
         backend=default_backend()
     )
 
-#파일에 저장된 공개 키를 읽어오기
+encrypted = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print("")
+print(encrypted)
+
+    
+
+
+##private_key = rsa.generate_private_key(
+##    public_exponent=65537,
+##    key_size=2048,
+##    backend=default_backend()
+##)
+##
+##public_key = private_key.public_key() ## rsa 공개키 생성
+##
+##print(public_key)
+##
+##encrypted = public_key.encrypt(
+##    message,
+##    padding.OAEP(
+##        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+##        algorithm=hashes.SHA256(),
+##        label=None
+##    )
+##)
+##print("")
+##print(encrypted)
+
+##pem = private_key.private_bytes(
+##    enconding = serialization.Encoding.PEM,
+##    format = serialization.PrivateFormat.PKCS8,
+##    encryption_algorithm = serialization.NoEncryption()
+##    )
+##with open('private_key.pem','wb')as f:
+##    f.write(pem)
+#### 개인 키를 파일에 저장
+##
+##with open("private_key.pem","rb")as key_file:
+##    private_key = serialization.load_pem_private_key(
+##        key_file.read(),
+##        password=None,
+##        backend=default_backend()
+##    )
+####파일에 저장된 개인 키를 읽어오기
+##
+##pem = public_key.public_bytes(
+##    encoding=serialization.Encoding.PEM,
+##    format=serialization.PublicFormat.SubjectPublicKeyInfo
+##)
+##
+##with open('public_key.pem','wb')as f:
+##    f.write(pem)
+####공개 키를 파일에 저장
+##
+##with open("public_key.pem","rb")as key_file:
+##    publick_key = serialization.load_pem_public_key(
+##        key_file.read(),
+##        backend=default_backend()
+##        )
+####파일에 저장된 개인 키를 읽어오기 
+
+##
+### #파일에 저장된 공개 키를 읽어오기
+##
